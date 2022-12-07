b6818b47533e9d4bc3af9c8e7b6900e741565aa0
joseph243 <josephvanderzwart@protonmail.com>
2022-12-07 08:25:07 -0700

    removed unirest , added new http response parsing

--- src/com/josephvanderzwart/PictureOfTheDay.java
+++ src/com/josephvanderzwart/PictureOfTheDay.java
@@ -1,32 +1,59 @@
 package com.josephvanderzwart;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PictureOfTheDay {
 
-    private String date;
-    private String url;
-    private String infoText;
+    private Map<String, String> attributes = new HashMap<String, String>();
 
-    public String getUrl() {
-        return url;
+    public String[] getAllAttributes() {
+        return (String[]) attributes.keySet().toArray();
     }
 
-    public void setUrl(String url) {
-        this.url = url;
+    public String getValue(String inAttribute)
+    {
+        return attributes.get(inAttribute);
     }
 
-    public String getInfoText() {
-        return infoText;
+    public PictureOfTheDay() {
     }
 
-    public void setInfoText(String infoText) {
-        this.infoText = infoText;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
+    public void parseHTTPresponse (String responseBody) {
+        responseBody = responseBody.substring(1, responseBody.length()-2);
+        String aKey = "";
+        String aVal = "";
+        String aObj = "";
+        boolean inObj = false;
+        for (Character c : responseBody.toCharArray()) {
+            if (c == '"')
+            {
+                //are we currently inside quotes?
+                inObj = !inObj;
+            }
+            if (inObj && c != '"')
+            {
+                aObj = aObj + c;
+            }
+            else
+            {
+                if (aKey == "")
+                {
+                    aKey = aObj;
+                    aObj = "";
+                }
+                else
+                {
+                    aVal = aObj;
+                    aObj = "";
+                }
+            }
+            if (!inObj && aKey != "" && aVal != "")
+            {
+                attributes.put(aKey, aVal);
+                aKey = "";
+                aVal = "";
+            }
+        }
     }
 }
